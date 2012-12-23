2640d089d7cff60ad271f094fe5841a2cdbe027a
Orpheon <alex.liebendorf@gmail.com>
2012-12-23 17:21:01 +0100

    Added general pipe class.

---
+++ src/Engine/Pipe.java
@@ -0,0 +1,47 @@
+package Engine;
+
+public class Pipe
+{
+	private static int counter;
+	private int index;
+	private int type;
+	
+	private double inner_buffer[] = new double[Engine.Constants.SNAPSHOT_SIZE];
+	private Module input;
+	private Module output;
+	
+	public Pipe()
+	{
+		index = counter++;
+	}
+	
+	public void set_input(Module module)
+	{
+		input = module;
+	}
+	
+	public void set_output(Module module)
+	{
+		output = module;
+	}
+	
+	public Module get_input()
+	{
+		return input;
+	}
+	
+	public Module get_output()
+	{
+		return output;
+	}
+	
+	public int get_index()
+	{
+		return index;
+	}
+
+	public int get_type()
+	{
+		return type;
+	}
+}
