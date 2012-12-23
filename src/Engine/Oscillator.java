1f9cc43c06ec6ebac81823503196048c0139eba8
Orpheon <alex.liebendorf@gmail.com>
2012-12-23 23:13:53 +0100

    Commented out old get_sound function, it also got hit by a few search/replaces

--- src/Engine/Oscillator.java
+++ src/Engine/Oscillator.java
@@ -4,51 +4,71 @@ package Engine;
  *
  */
 // TODO: Make this abstract
-public class Oscillator
+public abstract class Oscillator extends Module
 {
-	private double frequency = 1;
-	private double phase_offset = 0.0;
-
-	private double period = Engine.Constants.pi_times_2;
-	private double current_position = 0.0;
+	protected double frequency = 1;
+	protected double phase_offset = 0.0;
 	
-	private int samplerate = 1;
-	private double samplelength = 1;
+	protected double period = Engine.Constants.pi_times_2;
+	protected double current_position = 0.0;
+	
+	protected final int FREQUENCY_PIPE = 0;
+	protected final int PHASE_PIPE = 1;
+	
+	protected abstract double get_value(double position);
 	
 	public Oscillator(double frequency, double phase_offset, int samplerate)
 	{
-		this.set_frequency(frequency);
-		this.set_period(Functions.get_period(frequency));
-		this.current_position = 0.0;
-		this.set_phase(phase_offset);
-		this.set_samplerate(samplerate);
+		super();
+		
+		set_frequency(frequency);
+		set_period(1/frequency);
+		current_position = 0.0;
+		set_phase(phase_offset);
+		
+		NUM_INPUT_PIPES = 2;
+		NUM_OUTPUT_PIPES = 1;
 	}
-	
-	public byte[] get_sound(int num_samples, int sample_size)
+
+	public void get_sound()
+	{
+		for (int i=0; i<Engine.Constants.SNAPSHOT_SIZE; i++)
+		{
+			set_frequency(input_pipes[FREQUENCY_PIPE].inner_buffer[i]);
+			set_phase(input_pipes[PHASE_PIPE].inner_buffer[i]);
+
+			output_pipes[PHASE_PIPE].inner_buffer[i] = get_value(current_position);
+			
+			current_position += frequency;
+		}
+	}
+
+	// TODO: Get rid of this function
+/*	public byte[] get_sound(int num_samples, int sample_size)
 	{
 		int output_length = num_samples*sample_size;
 		double sample;
 
 //		System.out.println("\nData:");
-//		System.out.println(this.frequency);
-//		System.out.println(this.period);
-//		System.out.println(this.phase_offset);
-//		System.out.println(this.current_position);
-//		System.out.println(this.samplerate);
-//		System.out.println(this.samplelength);
+//		System.out.println(frequency);
+//		System.out.println(period);
+//		System.out.println(phase_offset);
+//		System.out.println(current_position);
+//		System.out.println(samplerate);
+//		System.out.println(samplelength);
 
 		byte[] output;
 		output = new byte[output_length];
 
 		for (int i=0; i<output_length; i+=sample_size)
 		{
-			sample = this.get_value(this.current_position);
+			sample = get_value(current_position);
 //			System.out.println("\nNew stuff:");
 //			System.out.println("i: "+i+" byte[] length: "+output.length+" num_samples: "+num_samples+" output_length: "+output_length);
-			this.current_position += this.samplelength * this.frequency * 6.28318530718;
-//			while (this.current_position > this.period)
+			current_position += samplelength * frequency * 6.28318530718;
+//			while (current_position > period)
 //			{
-//				this.current_position -= this.period;
+//				current_position -= period;
 //			}
 			// Then convert "sample" into a byte array and copy that to the output buffer
 			// FIXME: This is pretty inefficient, a casting for every number, is it possible to convert the whole array at once?
@@ -56,7 +76,7 @@ public class Oscillator
 		}
 
 		return output;
-	}
+	}*/
 
 	public double get_frequency()
 	{
