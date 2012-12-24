4116a9240640535d47c749e9897fc74d93cf33ba
Orpheon <alex.liebendorf@gmail.com>
2012-12-24 16:44:12 +0100

    Changed a bit the python debugger (used it quite a lot)

--- debugger.py
+++ debugger.py
@@ -1,4 +1,4 @@
-# Program to open and analyse java output in a language I know (and love)
+# Program to open and analyse java buffer output in a language I know
 from __future__ import division, print_function
 
 import struct
@@ -12,11 +12,14 @@ offset = 1
 
 while len(data) > 0:
     a = struct.unpack_from("<h", data)[0]
+    print("First: ", a)
     a /= 32767.0 # Max short size ( = normalize the shorts from -1 to 1)
+    print("Second: ", a)
     data = data[struct.calcsize("<h"):]
     #print(a)
     image.putpixel((offset, int(50 + 50*a)), (0, 255, 0))
     print(offset, int(50 + 50*a))
     offset += 1
+    print("")
 
 image.save("debug.png")
