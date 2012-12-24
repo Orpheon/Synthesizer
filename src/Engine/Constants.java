41e41c85e3bb152edf80b82f6b9b219ce66163d1
Orpheon <alex.liebendorf@gmail.com>
2012-12-24 16:41:23 +0100

    Improved a bit two constants.

--- src/Engine/Constants.java
+++ src/Engine/Constants.java
@@ -7,13 +7,12 @@ public class Constants
     // Audio sample size in bytes
     public final static int SAMPLE_SIZE = 2;
     // Size of a single snapshot buffer
-    // FIXME: I don't like this cast, but java seems to want it. Beware.
-    public final static int SNAPSHOT_SIZE = (int) Math.round((1/30.0)*SAMPLING_RATE*SAMPLE_SIZE);
+    public final static int SNAPSHOT_SIZE = 512;
     // 2*pi
     public final static double pi_times_2 = 6.28318530718;
     
     // Module type constants
-    public final static int OSCILLATOR = 1;
+    public final static int SINE_OSCILLATOR = 1;
     
     // Pipe type constants
     public final static int SOUND_PIPE = 1;
