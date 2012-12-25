c68da6185c8acb71b5745f5e08b064c0585cd9d6
Orpheon <alex.liebendorf@gmail.com>
2012-12-25 17:34:10 +0100

    Created simulation of 3 detuned saw oscillators which seems to work.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,6 +2,8 @@ package Main;
 
 import javax.sound.sampled.LineUnavailableException;
 
+import Modules.Oscillator;
+
 
 /**
  * Basic class that calls both the engine and the gui
@@ -22,8 +24,40 @@ public class Main
 	{
 		Engine.EngineMaster engine = new Engine.EngineMaster();
 		GUI.MainWindow window = new GUI.MainWindow();
-		window.createWindow(null);
-		engine.add_oscillator(440, 0, Modules.Oscillator.SINE_WAVE);
+		//window.createWindow(null);
+		
+		// Hardcoded situation
+		Modules.Splitter source = (Modules.Splitter) engine.add_module(Engine.Constants.MODULE_SPLITTER);
+		source.set_num_outputs(3);
+		// Yes, engine.input is the source pipe, that was a naming mistake I'll fix later
+		source.connect_input(engine.input, Modules.Splitter.INPUT_PIPE);
+		
+		Modules.Oscillator osc1, osc2, osc3;
+		osc1 = (Modules.Oscillator) engine.add_module(Engine.Constants.MODULE_OSCILLATOR);
+		osc2 = (Modules.Oscillator) engine.add_module(Engine.Constants.MODULE_OSCILLATOR);
+		osc3 = (Modules.Oscillator) engine.add_module(Engine.Constants.MODULE_OSCILLATOR);
+		
+		osc1.set_detune(3);
+		osc2.set_detune(-3);
+		
+		osc1.set_osctype(Modules.Oscillator.SAW_WAVE);
+		osc2.set_osctype(Modules.Oscillator.SAW_WAVE);
+		osc3.set_osctype(Modules.Oscillator.SAW_WAVE);
+		
+		engine.connect_modules(source, 0, osc1, Modules.Oscillator.FREQUENCY_PIPE);
+		engine.connect_modules(source, 1, osc2, Modules.Oscillator.FREQUENCY_PIPE);
+		engine.connect_modules(source, 2, osc3, Modules.Oscillator.FREQUENCY_PIPE);
+		
+		Modules.Merger adder = (Modules.Merger) engine.add_module(Engine.Constants.MODULE_MERGER);
+		adder.set_num_inputs(3);
+		adder.set_operation(Modules.Merger.ADDITION);
+		
+		engine.connect_modules(osc1, Modules.Oscillator.OUTPUT_PIPE, adder, 0);
+		engine.connect_modules(osc2, Modules.Oscillator.OUTPUT_PIPE, adder, 1);
+		engine.connect_modules(osc3, Modules.Oscillator.OUTPUT_PIPE, adder, 2);
+		
+		adder.connect_output(engine.output, Modules.Merger.OUTPUT_PIPE);
+
 		engine.start_playing(440);
 		while (true)
 		{
