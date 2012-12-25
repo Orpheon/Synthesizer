f696d815c3ba62265d618376684e0a105ba091dc
Orpheon <alex.liebendorf@gmail.com>
2012-12-25 16:47:50 +0100

    Did same for Splitter (see previous commit)

--- src/Modules/Splitter.java
+++ src/Modules/Splitter.java
@@ -7,6 +7,17 @@ import Engine.Constants;
 
 public class Splitter extends Module
 {
+	public Splitter(EngineMaster engine)
+	{
+		super(engine);
+		
+		NUM_INPUT_PIPES = 1;
+		NUM_OUTPUT_PIPES = 2;
+		
+		input_pipes = new Pipe[NUM_INPUT_PIPES];
+		output_pipes = new Pipe[NUM_OUTPUT_PIPES];
+	}
+	
 	public Splitter(EngineMaster engine, int num_outputs)
 	{
 		super(engine);
@@ -33,5 +44,32 @@ public class Splitter extends Module
 			}
 		}
 	}
-
+	
+	public int get_num_outputs()
+	{
+		return NUM_OUTPUT_PIPES;
+	}
+	
+	public void set_num_outputs(int num)
+	{
+		// No need to change anything if there's no change
+		if (num != NUM_OUTPUT_PIPES)
+		{
+			// If we want to lower the number of connections, then disconnect all the superfluous ones
+			if (NUM_OUTPUT_PIPES > num)
+			{ 
+				for (int i=num; i<NUM_OUTPUT_PIPES; i++)
+				{
+					if (output_pipes[i] != null)
+					{
+						disconnect_output(i);
+					}
+				}
+			}
+			NUM_OUTPUT_PIPES = num;
+			Pipe[] tmp = new Pipe[NUM_OUTPUT_PIPES];
+			System.arraycopy(output_pipes, 0, tmp, 0, Math.min(output_pipes.length, NUM_OUTPUT_PIPES));
+			output_pipes = tmp;
+		}
+	}
 }
