fc988cc142ce0dfe4851a10e6c45650e560ab639
Orpheon <alex.liebendorf@gmail.com>
2013-10-06 16:31:01 +0200

    Added and fixed FFT and IFFT functions.
    Finally.

--- src/Engine/Functions.java
+++ src/Engine/Functions.java
@@ -59,4 +59,156 @@ public class Functions
         	System.out.println(ex);
         }
       }
+    
+    // Created from the python line
+    // return sum(1<<(numbits-1-i) for i in range(numbits) if x>>i&1)
+    // Also from StackOverflow
+    static int bit_reverse(int x, int numbits)
+    {
+    	int output = 0;
+    	for (int i=0; i<numbits; i++)
+    	{
+    		if (((x>>i)&1) == 1)
+    		{
+    			output += 1<<(numbits-1-i);
+    		}
+    	}
+    	return output;
+    }
+	
+    // FFT Algorithm
+    // Main resource: http://cnx.org/content/m12016/latest/
+    static double[] fft(double[] input)
+    {
+    	// Input arrays (algorithm is in-place)
+    	double[] real = input;
+    	double[] imag = new double[input.length];
+    	
+    	// First do order switching (bit reversal)
+    	int j = 0, log2N;
+    	double tmp;
+    	// Assuming the input was a power of two, log2N should be a whole number
+    	log2N = (int) (Math.log(real.length)/Math.log(2));
+    	for (int i=0; i<real.length; i++)
+    	{
+    		j = bit_reverse(i, log2N);
+    		if (i < j)
+    		{
+    			// We haven't reversed these two numbers yet
+    			tmp = real[i];
+    			real[i] = real[j];
+    			real[j] = tmp;
+    		}
+    	}
+    	
+    	// Do the actual fft (see "Butterfly calculation")
+    	double offset, angle;
+    	double c, s, tmp_real, tmp_imag;
+    	for (int block_size=1; block_size<real.length; block_size*=2)
+    	{
+    		// In theory it should be offset = 2pi/2*block_size
+    		offset = Math.PI/block_size;
+    		angle = 0;
+    		
+    		for (int block=0; block<block_size; block++)
+    		{
+    			c = Math.cos(angle);
+    			s = Math.sin(angle);
+    			// TODO: Find out why a minus. Apparently twiddle factor is = e^(-i*2pi*f), and not e^(i*2pi*f)
+    			angle -= offset;
+    			
+    			for (int k=block; k<real.length; k+=block_size*2)
+    			{
+    				// Def: G(k) = x[k] + iy[k] (Block 1), H(k) = x[k+block_size] + iy[k+block_size] (Block 2)
+    				// Complex multiplication of H(k) and W
+    				tmp_real = c*real[k+block_size] - s*imag[k+block_size];// a1*a2 - b1*b2
+    				tmp_imag = c*imag[k+block_size] + s*real[k+block_size];// i(a1*b2 + a2*b1)
+    				
+    				// H(k) = G(k) - (H(k) * W) 
+    				real[k+block_size] = real[k] - tmp_real;
+    				imag[k+block_size] = imag[k] - tmp_imag;
+    				
+    				// G(k) = G(k) + (H(k) * W)
+    				real[k] += tmp_real;
+    				imag[k] += tmp_imag;
+    			}
+    		}
+    	}
+    	
+    	// real is in the input, return the phase through the imag array
+    	return imag;
+    }
+    
+    // IFFT Algorithm
+    // Derived from above
+    static void ifft(double[] amplitude, double[] phase)
+    {
+    	// Input arrays (algorithm is in-place)
+    	// Inverse real and imaginary --> FFT becomes IFFT
+    	// Proof: http://www.embedded.com/design/embedded/4210789/DSP-Tricks--Computing-inverse-FFTs-using-the-forward-FFT (Method #2)
+    	double[] real = phase;
+    	double[] imag = amplitude;
+    	
+    	// First do order switching (bit reversal)
+    	int j = 0, log2N;
+    	double tmp;
+    	// Assuming the input was a power of two, log2N should be a whole number
+    	log2N = (int) (Math.log(real.length)/Math.log(2));
+    	for (int i=0; i<real.length; i++)
+    	{
+    		j = bit_reverse(i, log2N);
+    		if (i < j)
+    		{
+    			// We haven't reversed these two numbers yet
+    			tmp = real[i];
+    			real[i] = real[j];
+    			real[j] = tmp;
+    			
+    			// Notice the minus signs - This conjugates the entire thing which makes the function behave like a IFFT
+    			tmp = imag[i];
+    			imag[i] = imag[j];
+    			imag[j] = tmp;
+    		}
+    	}
+    	
+    	// Do the actual fft (see "Butterfly calculation")
+    	double offset, angle;
+    	double c, s, tmp_real, tmp_imag;
+    	for (int block_size=1; block_size<real.length; block_size*=2)
+    	{
+    		// In theory it should be offset = 2pi/2*block_size
+    		offset = Math.PI/block_size;
+    		angle = 0;
+    		
+    		for (int block=0; block<block_size; block++)
+    		{
+    			c = Math.cos(angle);
+    			s = Math.sin(angle);
+    			angle -= offset;
+    			
+    			for (int k=block; k<real.length; k+=block_size*2)
+    			{
+    				// Def: G(k) = x[k] + iy[k] (Block 1), H(k) = x[k+block_size] + iy[k+block_size] (Block 2)
+    				// Complex multiplication of H(k) and W
+    				tmp_real = c*real[k+block_size] - s*imag[k+block_size];// a1*a2 - b1*b2
+    				tmp_imag = c*imag[k+block_size] + s*real[k+block_size];// i(a1*b2 + a2*b1)
+    				
+    				// H(k) = G(k) - (H(k) * W) 
+    				real[k+block_size] = real[k] - tmp_real;
+    				imag[k+block_size] = imag[k] - tmp_imag;
+    				
+    				// G(k) = G(k) + (H(k) * W)
+    				real[k] += tmp_real;
+    				imag[k] += tmp_imag;
+    			}
+    		}
+    	}
+    	
+    	// Inefficient, but easy way to scale back
+    	for (int i=0; i<real.length; i++)
+    	{
+    		real[i] /= real.length;
+    		imag[i] /= imag.length;
+    	}
+    }
 }
