feb5b111e11c3f062a6d2cb66ca41b6cb2ad0065
Orpheon <alex.liebendorf@gmail.com>
2013-10-07 10:42:27 +0200

    Overhauled some aspects of module structure.

--- src/Modules/RangeModifier.java
+++ src/Modules/RangeModifier.java
@@ -9,17 +9,18 @@ import Engine.Pipe;
 
 public class RangeModifier extends Module
 {
-	private double[] range_in;
-	private double[] range_out;
-	
-	private static final int INPUT_PIPE = 0;
-	private static final int OUTPUT_PIPE = 0;
+	private static final int SIGNAL_INPUT = 0;
+	private static final int MIN_INPUT = 1;
+	private static final int MAX_INPUT = 2;
+	private static final int MIN_OUTPUT = 3;
+	private static final int MAX_OUTPUT = 4;
+	private static final int SIGNAL_OUTPUT = 0;
 	
 	public RangeModifier(Container container)
 	{
 		super(container);
 		
-		NUM_INPUT_PIPES = 1;
+		NUM_INPUT_PIPES = 5;
 		NUM_OUTPUT_PIPES = 1;
 		
 		input_pipes = new Pipe[NUM_INPUT_PIPES];
@@ -27,79 +28,51 @@ public class RangeModifier extends Module
 		
 		input_pipe_names = new String[NUM_INPUT_PIPES];
 		output_pipe_names = new String[NUM_OUTPUT_PIPES];
-		input_pipe_names[0] = "Input";
-		output_pipe_names[0] = "Scaled output";
+		input_pipe_names[SIGNAL_INPUT] = "Signal Input";
+		input_pipe_names[MIN_INPUT] = "Min input range";
+		input_pipe_names[MAX_INPUT] = "Max input range";
+		input_pipe_names[MIN_OUTPUT] = "Min output range";
+		input_pipe_names[MAX_OUTPUT] = "Max output range";
+		output_pipe_names[SIGNAL_OUTPUT] = "Scaled output";
 		
 		module_type = Engine.Constants.MODULE_RANGEMODIFIER;
 		
 		MODULE_NAME = "Range Modifier";
-		// To store the ranges involved
-		range_in = new double[2];
-		range_out = new double[2];
 	}
 
 	@Override
 	public void run(Engine.EngineMaster engine, int channel)
 	{
-		if (input_pipes[INPUT_PIPE] != null && output_pipes[OUTPUT_PIPE] != null)
+		boolean everything_connected = true;
+		for (int i=0; i<NUM_INPUT_PIPES; i++)
 		{
-			if (input_pipes[INPUT_PIPE].get_type() != output_pipes[OUTPUT_PIPE].get_type())
+			if (input_pipes[i] == null)
 			{
-				System.out.println("Error in RangeModifier "+index+"; Input and output pipes have different types.");
-			}
-			double ratio, offset;
-			// Normalizing
-			ratio = (range_out[1] - range_out[0]) / (range_in[1] - range_in[0]);
-			offset = range_out[0] - (range_in[0] * ratio);
-			if (input_pipes[INPUT_PIPE].get_type() == Engine.Constants.MONO)
-			{
-				// Mono
-				for (int i=0; i<Engine.Constants.SNAPSHOT_SIZE; i++)
-				{
-					output_pipes[OUTPUT_PIPE].get_pipe(channel)[0][i] = ratio * input_pipes[INPUT_PIPE].get_pipe(channel)[0][i] + offset;
-				}
-			}
-			else
-			{
-				// Stereo
-				for (int i=0; i<Engine.Constants.SNAPSHOT_SIZE; i++)
-				{
-					output_pipes[OUTPUT_PIPE].get_pipe(channel)[0][i] = ratio * input_pipes[INPUT_PIPE].get_pipe(channel)[0][i] + offset;
-					output_pipes[OUTPUT_PIPE].get_pipe(channel)[1][i] = ratio * input_pipes[INPUT_PIPE].get_pipe(channel)[1][i] + offset;
-				}
+				everything_connected = false;
 			}
 		}
-	}
-
-	public double[] get_range_in()
-	{
-		return range_in;
-	}
-
-	public void set_range_in(double[] range_in)
-	{
-		this.range_in = range_in;
-	}
-	
-	public void set_range_in(double min, double max)
-	{
-		range_in[0] = min;
-		range_in[1] = max;
-	}
-
-	public double[] get_range_out()
-	{
-		return range_out;
-	}
-
-	public void set_range_out(double[] range_out)
-	{
-		this.range_out = range_out;
-	}
-	
-	public void set_range_out(double min, double max)
-	{
-		range_out[0] = min;
-		range_out[1] = max;
+		for (int i=0; i<NUM_OUTPUT_PIPES; i++)
+		{
+			if (output_pipes[i] == null)
+			{
+				everything_connected = false;
+			}
+		}
+		if (everything_connected)
+		{
+			double ratio, offset;
+			for (int side=0; side<audio_mode; side++)
+			{
+				for (int i=0; i<Engine.Constants.SNAPSHOT_SIZE; i++)
+				{
+				// Scaling
+				ratio = ((input_pipes[MAX_OUTPUT].get_pipe(channel)[side][i] - input_pipes[MIN_OUTPUT].get_pipe(channel)[side][i]) /
+						(input_pipes[MAX_INPUT].get_pipe(channel)[side][i] - input_pipes[MIN_INPUT].get_pipe(channel)[side][i]));
+				offset = input_pipes[MIN_OUTPUT].get_pipe(channel)[side][i] - (input_pipes[MIN_INPUT].get_pipe(channel)[side][i] * ratio);
+				output_pipes[SIGNAL_OUTPUT].get_pipe(channel)[side][i] = ratio * input_pipes[SIGNAL_INPUT].get_pipe(channel)[side][i] + offset;
+				}
+				
+			}
+		}
 	}
 }
